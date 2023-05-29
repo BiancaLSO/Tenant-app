4dc92fe3515808c7dd7706414c3db0734eee83cf
Adelina Radulescu <adelina.radulescu8@yahoo.com>
2023-05-29 14:54:06 +0200

    refresh issue array

--- screens/ChooseCategory.tsx
+++ screens/ChooseCategory.tsx
@@ -38,7 +38,7 @@ export default function ChooseCategory({ navigation }: MainProps) {
   }, [user]);
   useEffect(() => {
     dispatch(fetchUserIssues(userId));
-  }, [userId]);
+  }, [userId, userIssues]);
 
   const handleChooseCategory = (category: CategoryEntity) => {
     dispatch(setSelectedCategory(category));
